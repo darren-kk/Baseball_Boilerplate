197d8f2efa7e5d3b1c327c49cbdf24c566067411
darren-kk <rlawoghks10@gmail.com>
2022-11-24 20:10:22 +0900

    fix syntax and typo

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const startBtn = document.querySelector("#start");
 const throwinput = document.querySelector("#throw");
 const enterBtn = document.querySelector("#enter");
-const screen = document.querySelectorAll(".screen-div-ball")
-const outCount = document.querySelectorAll(".screen-div-count")
+const screen = document.querySelectorAll(".screen-div-ball");
+const outCount = document.querySelectorAll(".screen-div-count");
 const resetBtn = document.querySelector("#reset");
 const pitchArr = new Array(3);
-let answer = [];
+let randomNumber = [];
 let pitchBall = 0;
 let count = 0;
 
-
-
-function maker() {
+function createRandomNumber() {
   const makerArr = new Array(3)
   
   function random() {
@@ -23,65 +21,77 @@ function maker() {
   for (let i = 0; i < makerArr.length; i ++) {
     makerArr[i] = random();
   };
-  answer = makerArr;
-  return answer;
-};
-startBtn.addEventListener("click", maker);
 
-function pitch() {
+  randomNumber = makerArr;
+  
+  return randomNumber;
+}
+
+startBtn.addEventListener("click", createRandomNumber);
+
+function handleInput() {
   const pitcher = document.querySelector("#throw").value;
 
   if (pitcher > 999) {
     alert("숫자는 3자리여야 합니다!");
     document.querySelector("#throw").value = "";
-  } else if (isNaN(pitcher)) {
+  };
+  
+  if (isNaN(pitcher)) {
     alert("숫자를 입력해 주세요!");
     document.querySelector("#throw").value = "";
-  }
+  };
+
   pitchBall = pitcher;
+  
   return pitchBall;
 }
-throwinput.addEventListener('input', pitch);
+
+throwinput.addEventListener('input', handleInput);
 
 function compare() {
   const mapfn = (arg) => Number(arg);
-  const compareArr = Array.from(String(pitch()),mapfn);
+  const compareArr = Array.from(String(handleInput()),mapfn);
   let strike = 0;
   let ball = 0;
 
-  for (let j = 0; j < compareArr.length; j++) {
+  for (let i = 0; i < compareArr.length; i++) {
     
-    if (answer[j] === compareArr[j]) {
+    if (randomNumber[i] === compareArr[i]) {
       strike = strike + 1;
-    } else if (answer.includes(compareArr[j])) {
+    } else if (randomNumber.includes(compareArr[i])) {
       ball = ball + 1;
-    } 
+    }; 
+
   };
 
   screen[count].textContent = pitchBall;
-  outCount[count].textContent = `${strike}` + " strike " + `${ball}` + " ball";
+  outCount[count].textContent = `${strike} strike ${ball} ball`;
   count = count + 1;
   
   if (strike === 3) {
     alert("Homerun!");
     reset();
-  } else if (count === 10) {
+  }; 
+  
+  if (count === 10) {
     alert("Try again!");
     reset();
   };
 }
+
 enterBtn.addEventListener('click', compare);
 
 function reset() {
   count = 0; 
-  answer = [];
+  randomNumber = [];
   document.querySelector("#throw").value = "";
-  maker();
+  createRandomNumber();
   
-  for(let k = 0; k < screen.length; k++) {
-    screen[k].textContent = "";
-    outCount[k].textContent = "0 strike 0 ball"
+  for(let i = 0; i < screen.length; i++) {
+    screen[i].textContent = "";
+    outCount[i].textContent = "0 strike 0 ball"
   };
+}
 
-};
 resetBtn.addEventListener('click', reset);
